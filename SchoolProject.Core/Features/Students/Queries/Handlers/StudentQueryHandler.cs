af503d9d6eb123ef22eda6130766c6cf16504d73
MohamedKhaled <mk595458@gmail.com>
2025-06-02 17:31:07 +0300

    update

--- SchoolProject.Core/Features/Students/Queries/Handlers/StudentQueryHandler.cs
+++ SchoolProject.Core/Features/Students/Queries/Handlers/StudentQueryHandler.cs
@@ -40,7 +40,9 @@ namespace SchoolProject.Core.Features.Students.Queries.Handlers
             var studentList = await _studentService.GetStudentsListAsync();
 
             var studentListMapper = _mapper.Map<List<GetStudentListResponse>>(studentList);
-            return Success(studentListMapper);
+            var result = Success(studentListMapper);
+            result.Meta = new { Counte = studentListMapper.Count() };
+            return result;
         }
 
         public async Task<Response<GetSingleStudentResponse>> Handle(GetStudentByIDQuery request, CancellationToken cancellationToken)
@@ -54,9 +56,10 @@ namespace SchoolProject.Core.Features.Students.Queries.Handlers
 
         public async Task<PaginatedResult<GetStudentPaginatedListResponse>> Handle(GetStudentPaginatedListQuery request, CancellationToken cancellationToken)
         {
-            Expression<Func<Student, GetStudentPaginatedListResponse>> expresion = e => new GetStudentPaginatedListResponse(e.StudID, e.NameAr, e.Address, e.Department.DNameAr);
+            Expression<Func<Student, GetStudentPaginatedListResponse>> expresion = e => new GetStudentPaginatedListResponse(e.StudID, e.Localize(e.NameAr, e.NameEn), e.Address, e.Department.Localize(e.Department.DNameAr, e.Department.DNameEn));
             var FilterQuery = _studentService.FilterStudentPaginatedQuerable(request.OrderBy, request.Search);
             var PaginatedList = await FilterQuery.Select(expresion).ToPaginatedListAsync(request.PageNumber, request.PageSize);
+            PaginatedList.Meta = new { Count = PaginatedList.Data.Count() };
             return PaginatedList;
         }
         #endregion
